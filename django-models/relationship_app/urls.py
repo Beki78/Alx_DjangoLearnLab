bc112003bc8db24705ad6f0baa281e5a426a67d4
Baki78 <bakiget78@gmail.com>
2024-08-18 23:08:40 +0300

    16th

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', register, name='register'),  
 ]
