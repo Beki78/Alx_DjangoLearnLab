70681ce8774ef1efbaad5053f787dd368bb594fb
Baki78 <bakiget78@gmail.com>
2024-08-18 23:36:47 +0300

    23th

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, logout
 from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
+from .views import list_books
 from .models import Library
 from .models import Book
 from .models import UserProfile 
