4e657c92a4c3eeb92dea2dfb726c8073dfae06d7
Baki78 <bakiget78@gmail.com>
2024-08-18 23:04:55 +0300

    14th

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Library, Book
 
