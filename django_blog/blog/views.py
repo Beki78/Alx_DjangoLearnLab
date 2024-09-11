02faa8e9833f08bb480ac2df2693ebb4d4d3194b
Bereket Getachew <bakiget78@gmail.com>
2024-09-11 13:35:56 +0300

    3rd

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+# User Registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile View and Update
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        request.user.email = email
+        request.user.save()
+        messages.success(request, 'Your profile has been updated!')
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
