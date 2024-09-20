bb34c6aa5e8d39d463decc4b225c877ea170b4b9
Bereket Getachew <bakiget78@gmail.com>
2024-09-20 22:21:56 +0300

    11th

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
-from .models import Post, Comment
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment, Like  # Make sure to import Like
 from .serializers import PostSerializer, CommentSerializer
+from notifications.models import Notification  # Import Notification
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -10,28 +13,8 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-class FeedViewSet(viewsets.ViewSet):
-    permission_classes = [IsAuthenticated]
-
-    def list(self, request):
-        following_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
-
     def like_post(self, request, pk):
-        post = self.get_object()
+        post = get_object_or_404(Post, pk=pk)  # Correct way to retrieve the post
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
@@ -46,10 +29,27 @@ class PostViewSet(viewsets.ModelViewSet):
         return Response({"message": "You have already liked this post."})
 
     def unlike_post(self, request, pk):
-        post = self.get_object()
+        post = get_object_or_404(Post, pk=pk)  # Correct way to retrieve the post
         like = Like.objects.filter(user=request.user, post=post)
 
         if like.exists():
             like.delete()
             return Response({"message": "Post unliked."})
         return Response({"message": "You have not liked this post."})
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class FeedViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request):
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
