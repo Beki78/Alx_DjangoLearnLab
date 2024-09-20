11726efda10a7096e79ade93809552d1b1d8f8a0
Bereket Getachew <bakiget78@gmail.com>
2024-09-20 22:37:02 +0300

    14th

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -59,6 +59,17 @@ SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'social_media_api.urls'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': 'your_db_port',
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
