400b6daeb56a90f78c9aed5342559a5f2b5e9b55
Bereket Getachew <bakiget78@gmail.com>
2024-09-20 22:37:52 +0300

    15th

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -70,6 +70,12 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
